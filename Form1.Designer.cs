4216b781df85241b6146238dc118c9c0cf156201
SMartin96 <seanreimartin@hotmail.com>
2018-04-17 19:01:40 +0100

    Commit 1
    Created the GUI for the jukebox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CopyrightLabel = new System.Windows.Forms.Label();
+            this.GenreTextbox = new System.Windows.Forms.TextBox();
+            this.TrackListListbox = new System.Windows.Forms.ListBox();
+            this.HScrollBar = new System.Windows.Forms.HScrollBar();
+            this.NowPlayingTextbox = new System.Windows.Forms.TextBox();
+            this.SetupFormOpenButton = new System.Windows.Forms.Button();
+            this.AboutFormButton = new System.Windows.Forms.Button();
+            this.WaitingToPlayListbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // CopyrightLabel
+            // 
+            this.CopyrightLabel.AutoSize = true;
+            this.CopyrightLabel.Location = new System.Drawing.Point(372, 9);
+            this.CopyrightLabel.Name = "CopyrightLabel";
+            this.CopyrightLabel.Size = new System.Drawing.Size(211, 17);
+            this.CopyrightLabel.TabIndex = 0;
+            this.CopyrightLabel.Text = "Copy Right  © 2018 Sean Martin";
+            // 
+            // GenreTextbox
+            // 
+            this.GenreTextbox.Location = new System.Drawing.Point(216, 112);
+            this.GenreTextbox.Name = "GenreTextbox";
+            this.GenreTextbox.Size = new System.Drawing.Size(159, 22);
+            this.GenreTextbox.TabIndex = 1;
+            this.GenreTextbox.TextChanged += new System.EventHandler(this.GenreTextbox_TextChanged);
+            // 
+            // TrackListListbox
+            // 
+            this.TrackListListbox.FormattingEnabled = true;
+            this.TrackListListbox.ItemHeight = 16;
+            this.TrackListListbox.Location = new System.Drawing.Point(216, 134);
+            this.TrackListListbox.Name = "TrackListListbox";
+            this.TrackListListbox.Size = new System.Drawing.Size(159, 100);
+            this.TrackListListbox.TabIndex = 2;
+            this.TrackListListbox.SelectedIndexChanged += new System.EventHandler(this.TrackListListbox_SelectedIndexChanged);
+            // 
+            // HScrollBar
+            // 
+            this.HScrollBar.Location = new System.Drawing.Point(201, 237);
+            this.HScrollBar.Name = "HScrollBar";
+            this.HScrollBar.Size = new System.Drawing.Size(190, 27);
+            this.HScrollBar.TabIndex = 3;
+            this.HScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBar_Scroll);
+            // 
+            // NowPlayingTextbox
+            // 
+            this.NowPlayingTextbox.Location = new System.Drawing.Point(213, 267);
+            this.NowPlayingTextbox.Name = "NowPlayingTextbox";
+            this.NowPlayingTextbox.Size = new System.Drawing.Size(162, 22);
+            this.NowPlayingTextbox.TabIndex = 4;
+            this.NowPlayingTextbox.TextChanged += new System.EventHandler(this.NowPlayingTextbox_TextChanged);
+            // 
+            // SetupFormOpenButton
+            // 
+            this.SetupFormOpenButton.Location = new System.Drawing.Point(155, 475);
+            this.SetupFormOpenButton.Name = "SetupFormOpenButton";
+            this.SetupFormOpenButton.Size = new System.Drawing.Size(104, 24);
+            this.SetupFormOpenButton.TabIndex = 5;
+            this.SetupFormOpenButton.Text = "Setup";
+            this.SetupFormOpenButton.UseVisualStyleBackColor = true;
+            this.SetupFormOpenButton.Click += new System.EventHandler(this.SetupFormOpenButton_Click);
+            // 
+            // AboutFormButton
+            // 
+            this.AboutFormButton.Location = new System.Drawing.Point(336, 474);
+            this.AboutFormButton.Name = "AboutFormButton";
+            this.AboutFormButton.Size = new System.Drawing.Size(104, 26);
+            this.AboutFormButton.TabIndex = 6;
+            this.AboutFormButton.Text = "About";
+            this.AboutFormButton.UseVisualStyleBackColor = true;
+            this.AboutFormButton.Click += new System.EventHandler(this.AboutFormButton_Click);
+            // 
+            // WaitingToPlayListbox
+            // 
+            this.WaitingToPlayListbox.FormattingEnabled = true;
+            this.WaitingToPlayListbox.ItemHeight = 16;
+            this.WaitingToPlayListbox.Location = new System.Drawing.Point(213, 295);
+            this.WaitingToPlayListbox.Name = "WaitingToPlayListbox";
+            this.WaitingToPlayListbox.Size = new System.Drawing.Size(162, 148);
+            this.WaitingToPlayListbox.TabIndex = 7;
+            this.WaitingToPlayListbox.SelectedIndexChanged += new System.EventHandler(this.WaitingToPlayListbox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(595, 533);
+            this.Controls.Add(this.WaitingToPlayListbox);
+            this.Controls.Add(this.AboutFormButton);
+            this.Controls.Add(this.SetupFormOpenButton);
+            this.Controls.Add(this.NowPlayingTextbox);
+            this.Controls.Add(this.HScrollBar);
+            this.Controls.Add(this.TrackListListbox);
+            this.Controls.Add(this.GenreTextbox);
+            this.Controls.Add(this.CopyrightLabel);
+            this.Name = "Form1";
+            this.Text = "My Juke Box V1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label CopyrightLabel;
+        private System.Windows.Forms.TextBox GenreTextbox;
+        private System.Windows.Forms.ListBox TrackListListbox;
+        private System.Windows.Forms.HScrollBar HScrollBar;
+        private System.Windows.Forms.TextBox NowPlayingTextbox;
+        private System.Windows.Forms.Button SetupFormOpenButton;
+        private System.Windows.Forms.Button AboutFormButton;
+        private System.Windows.Forms.ListBox WaitingToPlayListbox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,40 @@ namespace JukeBoxv2
         {
             InitializeComponent();
         }
+
+        private void GenreTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackListListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void NowPlayingTextbox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void WaitingToPlayListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetupFormOpenButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutFormButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
